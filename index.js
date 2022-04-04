c2e6ebc02919352a2e142f76754149f79ca1cd5e
Mohammad <zia.ro@live.com>
2022-04-03 21:40:50 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+//code your solution here
+function superbowlWin(record){
+    function isWinningYear(year){
+      return year.result === "W";
+    }
+    const winningYearObject = record.find(isWinningYear);
+  
+    if (winningYearObject){
+      return winningYearObject.year
+    }
+  }
+
